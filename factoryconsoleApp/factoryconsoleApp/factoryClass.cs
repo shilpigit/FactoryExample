65406331bc306564da4a60858f865bac3f56c92a
VE170618A\admin <shilpi@virtualemployee.com>
2019-03-04 10:44:48 +0530

    Factory Pattern App

---
+++ factoryconsoleApp/factoryconsoleApp/factoryClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factoryconsoleApp
+{
+    interface Ifactory
+    {
+        void details();
+    }
+
+
+    class PermanentEmployee: Ifactory
+    {
+        public void details()
+        {
+            Console.WriteLine("Permanent Employee");
+        }
+    }
+
+    class TemporaryEmployee: Ifactory
+    {
+        public void details()
+        {
+            Console.WriteLine("Temporary Employee");
+        }
+    }
+
+    abstract class EmployeeFactory
+    {
+        public abstract Ifactory Factory(string Employee);
+    }
+    class factoryClass
+    {
+    }
+}
